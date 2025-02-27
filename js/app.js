e3974b5b5233de7a4eaec00a349eb0d4402327a8
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-02-27 12:30:58 +0100

    feat: second ex

--- js/app.js
+++ js/app.js
@@ -15,3 +15,20 @@ if (isNaN(firstNum) || isNaN(secondNum)){
     };
     console.log(`il numero più grande è ${currentNum}`);
 };
+
+// jsnack2
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const firstWord = prompt('Inserisci la prima parola.');
+const secondWord = prompt('Inserisci la seconda parola.');
+let currentWord;
+if (firstWord.length > secondWord.length){
+    currentWord = firstWord
+    console.log(`la parola più lunga è ${currentWord}`);
+} else if (firstWord.length < secondWord.length){
+    currentWord = secondWord
+    console.log(`la parola più lunga è ${currentWord}`);
+} else{
+    console.log(`le parole "${firstWord}" e "${secondWord}" hanno la stessa lunghezza`);
+}
